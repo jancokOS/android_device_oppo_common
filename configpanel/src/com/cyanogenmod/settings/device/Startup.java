efe66ff2a066bd5bd1837c5729a59980f8f18233
khabiburrokhman <khabiburrokhman@gmail.com>
2017-06-11 01:43:10 +0000

    Revert "common: setup node prefs on cmhw initialization"
    This reverts commit 7c014d65fa1d48e807c496d51f99b907ff5e36ed.

--- configpanel/src/com/cyanogenmod/settings/device/Startup.java
+++ configpanel/src/com/cyanogenmod/settings/device/Startup.java
@@ -52,7 +52,8 @@ public class Startup extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, final Intent intent) {
         final String action = intent.getAction();
-        if (cyanogenmod.content.Intent.ACTION_INITIALIZE_CM_HARDWARE.equals(action)) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(action)
+                || Intent.ACTION_PRE_BOOT_COMPLETED.equals(action)) {
             // Disable touchscreen gesture settings if needed
             if (!hasTouchscreenGestures()) {
                 disableComponent(context, TouchscreenGestureSettings.class.getName());
